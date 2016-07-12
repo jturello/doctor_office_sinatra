64c5246ecef7e6469d232a7aa22abddffcc80677
James Turello <jturello@yahoo.com>
2016-07-12 15:48:20 -0700

    add passing specs and code for Patient#save method setting & storing instance variables correctly

--- lib/patient.rb
+++ lib/patient.rb
@@ -1,11 +1,15 @@
 class Patient
 
-  attr_reader :dob, :name
+  attr_reader :dob, :name, :id
 
   define_method(:initialize) do |args|
     @name = args[:name]
     @dob = args[:dob]
   end
 
+  define_method(:save) do
+    result = DB.exec("INSERT INTO patients (name, dob) VALUES ('#{@name}', '#{@dob}') RETURNING id;")
+    @id = result.first().fetch("id").to_i()
+  end
 
 end
--- spec/patient_spec.rb
+++ spec/patient_spec.rb
@@ -13,5 +13,29 @@ describe('#initialize') do
   it('instatiates a patient object with a name') do
     expect(Patient.new({:name => 'Timmy Tommy', :dob => '1999-01-01'}).name).to eq('Timmy Tommy')
   end
+end
+
+describe('#save') do
+
+  it('saves a Patient with name in the database') do
+    patient = Patient.new({:name => 'Johny Jones', :dob => '1989-12-15'})
+    patient.save()
+    result = DB.exec("SELECT name FROM patients WHERE id = #{patient.id};")
+    expect(result.getvalue(0,0)).to eq(patient.name)
+  end
+
+  it('saves a Patient with dob to the database') do
+    patient = Patient.new({:name => 'Jules Verne', :dob => '1940-01-01'})
+    patient.save()
+    result = DB.exec("SELECT dob FROM patients WHERE id = #{patient.id};")
+    expect(result.getvalue(0,0)).to eq(patient.dob)
+  end
+
+  it("assigns the Patient object's id from the value returned from the database") do
+    patient = Patient.new({:name => 'Bob Jones', :dob => '1960-07-01'})
+    patient.save()
+    result = DB.exec("SELECT id FROM patients WHERE id = #{patient.id};")
+    expect(result.getvalue(0,0).to_i).to eq(patient.id)
+  end
 
 end
